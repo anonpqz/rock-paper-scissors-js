0e19fe7454e8ba37361b1a44f0b6125587464431
anonpqz <cthrowaway140@gmail.com>
2022-08-01 19:27:37 -1000

    Complete function 'playRound()'

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
+// get the computer's choice, return 'Rock', 'Paper', or 'Scissors'
 function getComputerChoice() {
     let choicenum = Math.floor(Math.random() * 3) + 1;
-    console.log(choicenum);
     switch(choicenum) {
         case 1:
             return "Rock";
@@ -8,7 +8,22 @@ function getComputerChoice() {
             return "Paper";
         case 3:
             return "Scissors";
-
-
     } 
+}
+
+// play a round of Rock, Paper, Scissors
+function playRound(playerSelection, computerSelection) {
+    const playerChoice = playerSelection.toLowerCase();
+    const computerChoice = computerSelection.toLowerCase();
+    if (playerChoice === computerChoice) return "Draw!";
+    if (playerChoice === "rock") {
+        if (computerChoice === "paper") return "You lose! Paper beats Rock.";
+        if (computerChoice === "scissors") return "You win! Rock beats Scissors.";
+    } else if (playerChoice === "paper") {
+        if (computerChoice === "rock") return "You win! Paper beats Rock.";
+        if (computerChoice === "scissors") return "You lose! Scissors beats Paper.";
+    } else if (playerChoice === "scissors") {
+        if (computerChoice === "rock") return "You lose! Rock beats Scissors.";
+        if (computerChoice === "paper") return "You win! Scissors beats Paper.";
+    }
 }
