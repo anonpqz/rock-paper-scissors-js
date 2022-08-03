30f9eb1986b4c23ce3ec7845b27e364e6264fe75
anonpqz <cthrowaway140@gmail.com>
2022-08-02 16:39:24 -1000

    Modify 'playRound()' function to return numeric values
    Modified the 'playRound()' function to return numeric values
    (rather than string values) for easier scoring in the main
    game loop function. Also created the main game loop function
    'game()'.

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function getComputerChoice() {
             return "Scissors";
     } 
 }
-
+/*
 // play a round of Rock, Paper, Scissors
 function playRound(playerSelection, computerSelection) {
     const playerChoice = playerSelection.toLowerCase();
@@ -25,5 +25,51 @@ function playRound(playerSelection, computerSelection) {
     } else if (playerChoice === "scissors") {
         if (computerChoice === "rock") return "You lose! Rock beats Scissors.";
         if (computerChoice === "paper") return "You win! Scissors beats Paper.";
+    }
+}
+*/
+
+function playRound(playerSelection, computerSelection) {
+    const playerChoice = playerSelection.toLowerCase();
+    const computerChoice = computerSelection.toLowerCase();
+
+    if (playerChoice === computerChoice) {
+        console.log("Draw! No score added, round not counted.");
+        return 2;
+    } else if (playerChoice === "rock") {
+        if (computerChoice === "paper") {
+            console.log("You lose! Paper beats Rock.");
+            return 0;
+        } else if (computerChoice === "scissors") {
+            console.log("You win! Rock beats Scissors.");
+            return 1;
+        }
+    } else if (playerChoice === "paper") {
+        if (computerChoice === "rock") {
+            console.log("You win! Paper beats Rock.");
+            return 1;
+        } else if (computerChoice === "scissors") {
+            console.log("You lose! Scissors beats Paper.");
+            return 0;
+        }
+    } else if (playerChoice === "scissors") {
+        if (computerChoice === "rock") {
+            console.log("You lose! Rock beats Scissors.");
+            return 0;
+        } else if (computerChoice === "paper") {
+            console.log("You win! Scissors beats Paper.");
+            return 1;
+        }
+    }
+}
+
+// main game loop
+function game() {
+    let playerWins, computerWins = 0;
+    console.log("Let's play . . . ROCK PAPER SCISSORS!! *organic applause*");
+    
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Enter your choice (rock, paper, or scissors): ").toLowerCase();
+        
     }
 }
